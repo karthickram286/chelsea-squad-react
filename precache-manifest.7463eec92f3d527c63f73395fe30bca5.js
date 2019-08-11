b8ae1dc7df577e2d8d1a6ed244394b3300479e1f
karthickram286 <karthick.r@zohocorp.com>
2019-08-11 10:53:12 +0530

    Updates

---
+++ precache-manifest.7463eec92f3d527c63f73395fe30bca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b34a8e338fb66cb9642e9d58f568ac3",
+    "url": "/chelsea-squad-react/index.html"
+  },
+  {
+    "revision": "1998a4e043ab638b61a5",
+    "url": "/chelsea-squad-react/static/css/main.19c39862.chunk.css"
+  },
+  {
+    "revision": "89b4349eb7d53a534724",
+    "url": "/chelsea-squad-react/static/js/2.86cc1835.chunk.js"
+  },
+  {
+    "revision": "1998a4e043ab638b61a5",
+    "url": "/chelsea-squad-react/static/js/main.9c7182ff.chunk.js"
+  },
+  {
+    "revision": "f5a91b79373c211e5a3f",
+    "url": "/chelsea-squad-react/static/js/runtime~main.1b5dc65b.js"
+  }
+]);
